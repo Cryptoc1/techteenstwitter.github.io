d7f0009c8832e43868ba1162462bc85721897259
Samuel Steele <steelesam72@gmail.com>
2015-12-08 18:43:16 -0500

    cleaned duplicate entry

--- static/members.js
+++ static/members.js
@@ -21,27 +21,4 @@ var Members = [{
         "linkedin": "https://www.linkedin.com/in/cryptoc1"
     },
     "portrait": "static/portraits/sam.jpg"
-}, {
-    "name": "Samuel Steele",
-    "skills": ["html", "css", "javascript"],
-    "interests": ["Movies", "Video Games", "Software", "Writing"],
-    "projects": [{
-        "title": "Zipf",
-        "url": "https://github.com/cryptoc1/zipf"
-    }, {
-        "title": "Maybe",
-        "url": "https://github.com/cryptoc1/maybe"
-    }, {
-        "title": "Hiroshima",
-        "url": "https://github.com/cryptoc1/hiroshima"
-    }],
-    "links": {
-        "web": "https://cryptoc1.github.io",
-        "twitter": "https://twitter.com/cryptoc1",
-        "github": "https://github.com/cryptoc1",
-        "instagram": "https://instagram.com/cryptoc1",
-        "facebook": "https://facebooks.com/cryptoc1",
-        "linkedin": "https://www.linkedin.com/in/cryptoc1"
-    },
-    "portrait": "static/portraits/sam.jpg"
 }]
